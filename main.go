6dc3b611c1c69cfbb58e9415a67861783b5b26a0
Matheus Lima <41559036+mateusfdl@users.noreply.github.com>
2021-11-05 13:01:14 -0300

    Removed useless license

--- main.go
+++ main.go
@@ -1,9 +1,3 @@
-
-   
-// Copyright 2014 The gocui Athors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
