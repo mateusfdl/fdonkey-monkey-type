5ee8985388506d4d2dea59cd296bf1c0333db0b1
Matheus Lima <matheus.limastack@outlook.com>
2023-01-25 00:21:48 -0300

    add main

--- main.go
+++ main.go
@@ -1,60 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/jroimartin/gocui"
-)
-
-var (
-	view string = "v1"
-	active  = 0
-)
-
-func setCurrentViewOnTop(g *gocui.Gui, name string) (*gocui.View, error) {
-	if _, err := g.SetCurrentView(name); err != nil {
-		return nil, err
-	}
-	return g.SetViewOnTop(name)
-}
-
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView(view, maxX/2-20, maxY/2, maxX/2+20, maxY/2+2); err != nil {
-		if err != gocui.ErrUnknownView {
-			return err
-		}
-		v.Editable = true
-
-		if _, err = setCurrentViewOnTop(g, view); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func quit(g *gocui.Gui, v *gocui.View) error {
-	return gocui.ErrQuit
-}
+import "github.com/mateusfdl/fdonkey-monkey-type/internal/model"
 
 func main() {
-	g, err := gocui.NewGui(gocui.OutputNormal)
-	if err != nil {
-		log.Panicln(err)
-	}
-	defer g.Close()
-
-	g.Highlight = true
-	g.Cursor = true
-	g.SelFgColor = gocui.ColorGreen
-
-	g.SetManagerFunc(layout)
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
-		log.Panicln(err)
-	}
+	model.Start()
 }
