0b3bb109bbe7f169e4db8f5ee52e212aed3bce3e
Matheus Lima <matheus.limastack@outlook.com>
2023-01-25 22:05:55 -0300

    add utils

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,7 +1,32 @@
 package utils
 
-import "github.com/thefuga/go-collections"
+import (
+	"math/rand"
+	"time"
 
-func [T any]shuffle(s []T) []T {
-	return collections.Shuffle(s)
+	"github.com/thefuga/go-collections"
+)
+
+type Text struct {
+	Words []string
+}
+
+func New(s []string) *Text {
+	return &Text{
+		Words: s,
+	}
+}
+
+func (s *Text) Shuffle() *Text {
+	rand.Seed(time.Now().UnixMicro())
+
+	s.Words = collections.Shuffle(s.Words)
+
+	return s
+}
+
+func (s *Text) Chunck(size int) *Text {
+	s.Words = collections.ForPage(s.Words, 1, size)
+
+	return s
 }
