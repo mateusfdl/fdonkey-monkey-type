b1285a713a4e6a4c69ff586ab2bffd81df423c4f
Matheus Lima <matheus.limastack@outlook.com>
2023-01-26 22:30:17 -0300

    add example default config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/viper"
 )
@@ -10,10 +12,16 @@ type Config interface {
 	Get(field string) interface{}
 }
 
+var (
+	FilePath = fmt.Sprintf("%v/.config/fdonkey", homeDir())
+	FileType = "toml"
+	FileName = "theme"
+)
+
 func init() {
-	viper.SetConfigName("theme")
-	viper.SetConfigType("toml")
-	viper.AddConfigPath("$HOME/.config/fdonkey")
+	viper.SetConfigName(FileName)
+	viper.SetConfigType(FileType)
+	viper.AddConfigPath(FilePath)
 }
 
 func LoadConfig() Config {
@@ -23,4 +31,35 @@ func LoadConfig() Config {
 	}
 
 	return viper.GetViper()
+}
+
+func CreateExampleConfigFile() {
+	err := os.MkdirAll(FilePath, os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile(fmt.Sprintf("%v/%v.%v", FilePath, FileName, FileType), exampleConfigFile(), 0660)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func homeDir() string {
+	path, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return path
+}
+
+func exampleConfigFile() []byte {
+	return []byte(`[theme]
+background = "#fff"
+success = "#000"
+fail = "#000"
+typed = "#000"
+`)
 }
