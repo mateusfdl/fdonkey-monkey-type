45df094920021fae3bc22bf8ca29548bc9c3211e
Matheus Lima <matheus.limastack@outlook.com>
2023-01-26 22:30:37 -0300

    add -generate-file flag to create an example of the config file

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "github.com/mateusfdl/fdonkey-monkey-type/internal/model"
+import (
+	"flag"
+
+	"github.com/mateusfdl/fdonkey-monkey-type/internal/config"
+	"github.com/mateusfdl/fdonkey-monkey-type/internal/model"
+)
+
+var (
+	generateFile = flag.Bool("generate-file", false, "generates a config file example")
+)
 
 func main() {
+	flag.Parse()
+	if *generateFile {
+		config.CreateExampleConfigFile()
+		return
+	}
+
 	model.Start()
 }
