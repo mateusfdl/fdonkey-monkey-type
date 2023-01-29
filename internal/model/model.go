276c9c08f29fd14fffb7b1048943b29788ddf45b
Matheus Lima <matheus.limastack@outlook.com>
2023-01-29 02:54:36 -0300

    removed initialModel func

--- internal/model/model.go
+++ internal/model/model.go
@@ -6,30 +6,24 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 
-	"github.com/mateusfdl/fdonkey-monkey-type/internal/text"
+	"github.com/mateusfdl/fdonkey-monkey-type/internal/theme"
+	"github.com/mateusfdl/fdonkey-monkey-type/internal/utils"
 	"github.com/mateusfdl/fdonkey-monkey-type/internal/window"
 )
 
-type model struct {
+type Model struct {
 	Text []rune
 
 	Typed []rune
+
+	Theme *theme.Theme
 }
 
-func initialModel() model {
-	text := text.LoadText()
-
-	return model{
-		Text:  []rune(text),
-		Typed: []rune{},
-	}
-}
-
-func (m model) Init() tea.Cmd {
+func (m Model) Init() tea.Cmd {
 	return nil
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		if msg.Type == tea.KeyCtrlC {
@@ -55,24 +49,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m model) View() string {
+func (m Model) View() string {
 	remaining := string(m.Text[len(m.Typed):])
 
 	var typed string
 	for i, c := range m.Typed {
 		if c == rune(m.Text[i]) {
-			typed += fmt.Sprintf("\x1B[28;2;249;38;114m%v\x1B[0m", string(c))
+			typed += utils.Sprintf(m.Theme.Typed, m.Theme.Background, string(c)).String()
+			//typed += fmt.Sprintf("\x1B[28;2;249;38;114m%v\x1B[0m", string(c))
 		} else {
-			typed += fmt.Sprintf("\x1B[38;2;249;38;114m%v\x1B[0m", string(m.Text[i]))
+			typed += utils.Sprintf(m.Theme.Failed, m.Theme.Background, string(m.Text[i])).String()
 		}
 
 	}
 
-	return window.NewWindow().Render(fmt.Sprintf("%s%s", typed, remaining))
+	return window.NewWindow().Render(fmt.Sprintf("%s%s", typed, utils.Sprintf(m.Theme.Font, m.Theme.Background, remaining).String()))
 }
 
-func Start() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+func Start(m Model) {
+	p := tea.NewProgram(m, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("err %v", err)
 		os.Exit(1)
