271e44494c0dd28b91a8fa637e8a2c0790eb6180
Matheus Lima <matheus.limastack@outlook.com>
2023-01-29 02:55:31 -0300

    add all fdonkey setup to main

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 
 	"github.com/mateusfdl/fdonkey-monkey-type/internal/config"
 	"github.com/mateusfdl/fdonkey-monkey-type/internal/model"
+	"github.com/mateusfdl/fdonkey-monkey-type/internal/text"
+	"github.com/mateusfdl/fdonkey-monkey-type/internal/theme"
 )
 
 var (
@@ -18,5 +21,16 @@ func main() {
 		return
 	}
 
-	model.Start()
+	t := text.LoadText()
+	theme := theme.New()
+
+	m := model.Model{
+		Text:  []rune(t),
+		Typed: []rune{},
+		Theme: theme,
+	}
+	fmt.Println(m.Theme.Font)
+	fmt.Println(m.Theme.Typed)
+	fmt.Println(m.Theme.Failed)
+	model.Start(m)
 }
